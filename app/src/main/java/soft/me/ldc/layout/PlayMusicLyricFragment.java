39fc7a3cf619e2fc297df54babfcd803968d7860
ldc456@sina.cn <ldc456@sina.cn>
2018-02-11 23:51:43 +0800

    update code

--- app/src/main/java/soft/me/ldc/layout/PlayMusicLyricFragment.java
+++ app/src/main/java/soft/me/ldc/layout/PlayMusicLyricFragment.java
@@ -2,19 +2,30 @@ package soft.me.ldc.layout;
 
 
 import android.os.Bundle;
+import android.support.annotation.ColorLong;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import soft.me.ldc.R;
 import soft.me.ldc.base.RootFragment;
+import soft.me.ldc.view.GRToastView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlayMusicLyricFragment extends RootFragment {
+
+    @BindView(R.id.PingIcon)
+    AppCompatImageView PingIcon;
 
     //构造函数
     public PlayMusicLyricFragment() {
@@ -45,6 +56,15 @@ public class PlayMusicLyricFragment extends RootFragment {
 
     @Override
     protected void Exception(Exception e) {
+        GRToastView.show(ctx, "系统异常", Toast.LENGTH_SHORT);
+    }
 
+    @OnClick({R.id.PingIcon})
+    public void ClickLisener(View view) {
+        switch (view.getId()) {
+            case R.id.PingIcon:
+                GRToastView.show(ctx, "这是一只猪!!", Toast.LENGTH_SHORT);
+                break;
+        }
     }
 }
